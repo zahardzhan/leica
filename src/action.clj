1410c8ea03789d5d25445021772d01c2abfc7ae8
Roman Zaharov <zahardzhan@gmail.com>
2010-01-29 20:09:38 +1000

    добавил подробную документацию в env и action

--- src/action.clj
+++ src/action.clj
@@ -1,7 +1,7 @@
 ;;; -*- mode: clojure; coding: utf-8 -*-
 ;;; authors: Roman Zaharov <zahardzhan@gmail.com>
 
-(ns #^{:doc "Базовые действия агентов."
+(ns #^{:doc "Базовые действия агента."
        :author "Роман Захаров"}
   action
   (:require [clojure.contrib.logging :as log])
@@ -9,7 +9,10 @@
 
 (in-ns 'action)
 
-(defn execute [ag action]
+(defn execute 
+  "Возвращает новое тело агента - результат выполнения агентом действия.
+  Действие выполняется в отдельном потоке, сам агент не изменяется."
+  [ag action]
   (with-deref [ag]
     (let [fag (future (assoc (((ag :actions) action) ag)
                         :action (if (#{:die :fail :pass} action)
@@ -20,20 +23,28 @@
         (log/debug (str (or (fag :name) (fag :address)) \space action \space 
                         (cond (dead? fag) "агент умер"
                               (fail? fag) "агент провалился"
-                              :else        "успешно")))
+                              :else       "успешно")))
         fag))))
 
-(defn percept 
+(defn percept
+  "Возвращает действие - результат восприятия программой агента тела агента,
+  окружения и самого `восприятия'. Восприятие передается программе агента в 
+  виде хэша."
   ([ag] (percept ag {}))
   ([ag perception] (with-deref [ag] ((ag :program) ag perception))))
 
 (defn percept-and-execute
+  "Агент выполняет действие, основываясь на восприятии."
   ([ag] (percept-and-execute ag {}))
   ([ag perception] (with-deref [ag]
                      (let [action (percept ag perception)]
                        (execute ag action)))))
 
 (defn after
+  "Возвращает true, если последнее действие агента совпадает с аргументом.
+  В качестве статуса `status' можно указать ключи:
+  :successful - удачное завершение последнего действия и
+  :failed - неудачное. "
   ([action ag] (with-deref [ag] (= action (:action ag))))
   ([status action ag] 
      (with-deref [ag]
@@ -42,11 +53,11 @@
                   :successful ((no fail?) ag)
                   :failed     (fail? ag))))))
 
-(defn pass [ag]
-  ag)
+(defn pass "Пустое действие - возвращается неизменное тело агента."
+  [ag] ag)
  
-(defn fail [ag]
-  (assoc ag :fail true))
+(defn fail "Неудачное действие - возвращается тело агента с маркером неудачи."
+  [ag] (assoc ag :fail true))
  
-(defn die [ag]
-  (assoc ag :alive false))
+(defn die "Смерть агента - возвращается тело агента с маркером смерти."
+  [ag] (assoc ag :alive false))
