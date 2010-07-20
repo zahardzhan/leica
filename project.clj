00ec287b0f25d5cd489f3490088fb0272ac9620b
Roman Zaharov <zahardzhan@gmail.com>
2010-07-20 12:28:07 +1100

    Change version. Add and remove some dependencies.

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
-(defproject leica "0.3.0-master-SNAPSHOT" 
-  :description "Multithreaded downloader for data.cod.ru"
+(defproject leica "0.4.0-master-SNAPSHOT" 
+  :description "Multithreaded downloader for data.cod.ru."
   :url "http://github.com/zahardzhan/leica"
   :autodoc {:name "Leica", :page-title "Leica API documentation"}
   :dependencies     [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                      [ahc-clj "0.1.1"]
-                     [commons-codec "1.4"]
-                     [commons-httpclient "3.1"]
-                     [commons-logging "1.1.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]                     
-                     [autodoc "0.7.0"]]
+                     [hooks "1.0.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :main leica)
