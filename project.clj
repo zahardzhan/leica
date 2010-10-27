7bd60d73ce403b956673516dcf5bae389f4eb26e
Roman Zaharov <zahardzhan@gmail.com>
2010-10-27 12:54:45 +1100

    New Leiningen and dependencies.

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "Multithreaded downloader for data.cod.ru."
   :url "http://github.com/zahardzhan/leica"
   :autodoc {:name "Leica", :page-title "Leica API documentation"}
-  :dependencies     [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                     [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies     [[org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]
+                     [com.twinql.clojure/clj-apache-http "2.2.0"]
                      [hooks "1.0.0"]
                      [fn    "1.0.0"]
                      [commons-codec      "1.4"]
