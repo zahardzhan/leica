e01c62478739d02463b15749749035d214b54c3b
Roman Zaharov <zahardzhan@gmail.com>
2010-11-02 13:59:07 +1000

    Remove shitty clojure HTTP library.

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :autodoc {:name "Leica", :page-title "Leica API documentation"}
   :dependencies     [[org.clojure/clojure "1.2.0"]
                      [org.clojure/clojure-contrib "1.2.0"]
-                     [com.twinql.clojure/clj-apache-http "2.2.0"]
                      [hooks "1.0.0"]
-                     [fn    "1.0.0"]
                      [commons-codec      "1.4"]
                      [commons-httpclient "3.1"]
                      [commons-logging    "1.1.1"]]
